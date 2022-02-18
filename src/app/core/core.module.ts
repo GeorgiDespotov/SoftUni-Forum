23a041a53bd2dcecf096866ab7c9ee1a047ce14d
Georgi Despotov <79579263+GeorgiDespotov@users.noreply.github.com>
2022-02-18 16:58:11 +0200

    Initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeatherComponent } from './heather/heather.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeatherComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
